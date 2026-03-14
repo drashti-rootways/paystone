0043ddc2cdf747f65fb7a18de6f5b4134f391dd5
Drashti Patel <drashti@rootways.com>
2026-03-14 12:45:41 +0530

    updated a index file

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -3,7 +3,7 @@ import { useFetcher } from "react-router";
 import { useAppBridge } from "@shopify/app-bridge-react";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
-import { getOrCreateShop } from "../server/shop.server";
+import {getOrCreateShop} from "../ server/shop.server"
 
 
 export const loader = async ({ request }) => {
